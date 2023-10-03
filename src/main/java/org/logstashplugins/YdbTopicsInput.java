26cc284e3a0db5fe6416fdc46750de8c2b78fd60
DevSMike <likashev.migha@yandex.ru>
2023-10-03 15:14:48 +0300

    feat: add logstash plugin code

--- src/main/java/org/logstashplugins/YdbTopicsInput.java
+++ src/main/java/org/logstashplugins/YdbTopicsInput.java
@@ -14,9 +14,9 @@ import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 
-// class name must match plugin name
-@LogstashPlugin(name="java_input_example")
-public class JavaInputExample implements Input {
+
+@LogstashPlugin(name="ydb_topics_input")
+public class YdbTopicsInput implements Input {
 
     public static final PluginConfigSpec<Long> EVENT_COUNT_CONFIG =
             PluginConfigSpec.numSetting("count", 3);
@@ -24,15 +24,13 @@ public class JavaInputExample implements Input {
     public static final PluginConfigSpec<String> PREFIX_CONFIG =
             PluginConfigSpec.stringSetting("prefix", "message");
 
-    private String id;
-    private long count;
-    private String prefix;
+    private final String id;
+    private final long count;
+    private final String prefix;
     private final CountDownLatch done = new CountDownLatch(1);
     private volatile boolean stopped;
 
-    // all plugins must provide a constructor that accepts id, Configuration, and Context
-    public JavaInputExample(String id, Configuration config, Context context) {
-        // constructors should validate configuration options
+    public YdbTopicsInput(String id, Configuration config, Context context) {
         this.id = id;
         count = config.get(EVENT_COUNT_CONFIG);
         prefix = config.get(PREFIX_CONFIG);
@@ -40,15 +38,6 @@ public class JavaInputExample implements Input {
 
     @Override
     public void start(Consumer<Map<String, Object>> consumer) {
-
-        // The start method should push Map<String, Object> instances to the supplied QueueWriter
-        // instance. Those will be converted to Event instances later in the Logstash event
-        // processing pipeline.
-        //
-        // Inputs that operate on unbounded streams of data or that poll indefinitely for new
-        // events should loop indefinitely until they receive a stop request. Inputs that produce
-        // a finite sequence of events should loop until that sequence is exhausted or until they
-        // receive a stop request, whichever comes first.
 
         int eventCount = 0;
         try {
@@ -65,17 +54,16 @@ public class JavaInputExample implements Input {
 
     @Override
     public void stop() {
-        stopped = true; // set flag to request cooperative stop of input
+        stopped = true;
     }
 
     @Override
     public void awaitStop() throws InterruptedException {
-        done.await(); // blocks until input has stopped
+        done.await();
     }
 
     @Override
     public Collection<PluginConfigSpec<?>> configSchema() {
-        // should return a list of all configuration options for this plugin
         return Arrays.asList(EVENT_COUNT_CONFIG, PREFIX_CONFIG);
     }
 
